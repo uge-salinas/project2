50e66e929bb32396c9befaebd98eee2aad05baca
mzclas <Mzclas@gmail.com>
2019-02-13 15:16:09 +0100

    agregadas tarjetas de parcelas

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 const Parcela = require("../models/Parcela");
@@ -9,7 +9,6 @@ const key = process.env.KEYMAP;
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
-
 
 //SIGN UP
 
@@ -23,17 +22,21 @@ router.post("/signup", (req, res, next) => {
   const email = req.body.email;
 
   if (username == "" || password == "" || email == "") {
-    res.render("auth/signup", { message: "Por favor complete todos los campos" })
-    return
+    res.render("auth/signup", {
+      message: "Por favor complete todos los campos"
+    });
+    return;
   }
 
   User.findOne({ username }, "username", (err, user) => {
     if (user !== null) {
-      res.render("auth/signup", { message: "Nombre de usuario no disponible, por favor elija uno nuevo" })
-      return
+      res.render("auth/signup", {
+        message: "Nombre de usuario no disponible, por favor elija uno nuevo"
+      });
+      return;
     }
 
-    if (err) return "Error de ejecución"
+    if (err) return "Error de ejecución";
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPass = bcrypt.hashSync(password, salt);
 
@@ -41,32 +44,36 @@ router.post("/signup", (req, res, next) => {
       username,
       password: hashPass,
       email
-    })
+    });
 
-    newUser.save()
+    newUser
+      .save()
       .then(() => res.redirect("/"))
-      .catch(err => console.log(err))
-  })
+      .catch(err => console.log(err));
+  });
 });
-
 
 //LOG IN
 
 router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
+  res.render("auth/login", { message: req.flash("error") });
 });
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/auth/member",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
-
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/auth/member",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 //MEMBER'S SITE
 
-router.get("/member", ensureLogin.ensureLoggedIn(), (req, res, next) => res.render("auth/member", { key }))
+router.get("/member", ensureLogin.ensureLoggedIn(), (req, res, next) =>
+  res.render("auth/member", { key })
+);
 
 router.post("/member", (req, res, next) => {
   const tipo = req.body.tipo;
@@ -79,18 +86,27 @@ router.post("/member", (req, res, next) => {
     dimensiones,
     venta,
     precio
-  })
+  });
 
-  nuevaParcela.save()
+  nuevaParcela
+    .save()
     .then(() => res.redirect("/"))
-    .catch(err => console.log(err))
-})
+    .catch(err => console.log(err));
+});
 
 //LOG OUT
 
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
+});
+
+//CARDS
+
+router.get("/card", ensureLogin.ensureLoggedIn(), (req, res) => {
+  Parcela.find().then(parcelas => {
+    res.render("auth/card", { parcelas });
+  });
 });
 
 module.exports = router;
